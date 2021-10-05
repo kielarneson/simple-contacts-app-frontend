10178fd9d237aad8883741c912d7997e0939d2bc
Kiel Arneson <kiel.arneson@gmail.com>
2021-10-05 13:33:04 -0500

    Reset home file to work on front end CRUD some more

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,123 +1,19 @@
 <template>
   <div class="home">
-    <h1>Create Contact</h1>
-    <div>
-      <div>
-        First Name:
-        <input type="text" v-model="newContactParams.first_name" />
-      </div>
-      <div>
-        Last Name:
-        <input type="text" v-model="newContactParams.last_name" />
-      </div>
-      <div>
-        Email:
-        <input type="text" v-model="newContactParams.email" />
-      </div>
-      <div>
-        Phone Number:
-        <input type="text" v-model="newContactParams.phone_number" />
-      </div>
-      <div>
-        Image URL:
-        <input type="text" v-model="newContactParams.image" />
-      </div>
-      <button v-on:click="createContacts()">Create</button>
-    </div>
-
-    <h1>Contacts</h1>
-    <div v-for="contact in contacts" v-bind:key="contact.id">
-      <h2>{{ contact.first_name }} {{ contact.last_name }}</h2>
-      <img :src="contact.image" alt="" />
-      <p>{{ contact.email }}</p>
-      <p>{{ contact.phone_number }}</p>
-      <button v-on:click="showContact(contact)">More Info</button>
-    </div>
-
-    <dialog id="contact-details">
-      <form method="dialog">
-        <h1>Contact Info</h1>
-        <div>
-          First Name:
-          <input type="text" v-model="currentContact.first_name" />
-        </div>
-        <div>
-          Last Name:
-          <input type="text" v-model="currentContact.last_name" />
-        </div>
-        <div>
-          Email:
-          <input type="text" v-model="currentContact.email" />
-        </div>
-        <div>
-          Phone Number:
-          <input type="text" v-model="currentContact.phone_number" />
-        </div>
-        <div>
-          Image URL:
-          <input type="text" v-model="currentContact.image" />
-        </div>
-        <button v-on:click="updateContact(currentContact)">Update</button>
-        <button v-on:click="deleteContact(currentContact)">Delete</button>
-        <button>Close</button>
-      </form>
-    </dialog>
+    <h1>{{ message }}</h1>
   </div>
 </template>
 
-<style>
-img {
-  width: 200px;
-}
-</style>
+<style></style>
 
 <script>
-import axios from "axios";
-
 export default {
   data: function () {
     return {
-      contacts: [],
-      newContactParams: {},
-      currentContact: {},
+      message: "Welcome to Vue.js!",
     };
   },
-  created: function () {
-    this.indexContacts();
-  },
-  methods: {
-    indexContacts: function () {
-      axios.get("/contacts").then((response) => {
-        console.log("Contacts index", response.data);
-        this.contacts = response.data;
-      });
-    },
-    createContacts: function () {
-      axios.post("/contacts.json", this.newContactParams).then((response) => {
-        console.log("Contacts create", response);
-        this.contacts.push(response.data);
-        this.newContactParams = {};
-      });
-    },
-    showContact: function (contact) {
-      this.currentContact = contact;
-      document.querySelector("#contact-details").showModal();
-    },
-    updateContact: function (contact) {
-      var updateContactParams = contact;
-      axios.patch(`/contacts/${contact.id}.json`, updateContactParams).then((response) => {
-        console.log("Update contact", response.data);
-        this.currentContact = {};
-      });
-    },
-    deleteContact: function (contact) {
-      this.currentContact = contact;
-      axios.delete(`/contacts/${contact.id}.json`).then((response) => {
-        console.log("Successfully deleted contact", response.data);
-        var index = this.contacts.indexOf(contact);
-        this.contacts.splice(index, 1);
-      });
-    },
-  },
+  created: function () {},
+  methods: {},
 };
 </script>
