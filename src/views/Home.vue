d11d83743bcf30d2a6361800e0cabf48d76a44b3
Kiel Arneson <kiel.arneson@gmail.com>
2021-10-05 10:59:24 -0500

    Added indexContacts and the associated html to display database info

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,40 @@
 <template>
   <div class="home">
-    <h1>{{ message }}</h1>
+    <h1>Contacts</h1>
+    <div v-for="contact in contacts" v-bind:key="contact.id">
+      <h2>{{ contact.first_name }} {{ contact.last_name }}</h2>
+      <img :src="contact.image" alt="" />
+      <p>{{ contact.email }}</p>
+      <p>{{ contact.phone_number }}</p>
+    </div>
   </div>
 </template>
 
-<style></style>
+<style>
+img {
+  width: 200px;
+}
+</style>
 
 <script>
+import axios from "axios";
+
 export default {
   data: function () {
     return {
-      message: "Welcome to Vue.js!",
+      contacts: [],
     };
   },
-  created: function () {},
-  methods: {},
+  created: function () {
+    this.indexContacts();
+  },
+  methods: {
+    indexContacts: function () {
+      axios.get("/contacts").then((response) => {
+        console.log("Photos index", response.data);
+        this.contacts = response.data;
+      });
+    },
+  },
 };
 </script>
