46f9685ac0727a304f7fe645d131f0d8ddb82220
Kiel Arneson <kiel.arneson@gmail.com>
2021-10-05 11:14:55 -0500

    Added create function and associated html to allow user to create new users

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,25 @@
 <template>
   <div class="home">
+    <h1>Create Contact</h1>
+    <div>
+      <div>
+        First Name:
+        <input type="text" v-model="newContactParams.first_name" />
+      </div>
+      <div>
+        Last Name:
+        <input type="text" v-model="newContactParams.last_name" />
+      </div>
+      <div>
+        Email:
+        <input type="text" v-model="newContactParams.email" />
+      </div>
+      <div>
+        Phone Number:
+        <input type="text" v-model="newContactParams.phone_number" />
+      </div>
+      <button v-on:click="createContacts()">Create</button>
+    </div>
     <h1>Contacts</h1>
     <div v-for="contact in contacts" v-bind:key="contact.id">
       <h2>{{ contact.first_name }} {{ contact.last_name }}</h2>
@@ -23,6 +43,7 @@ export default {
   data: function () {
     return {
       contacts: [],
+      newContactParams: {},
     };
   },
   created: function () {
@@ -31,8 +52,15 @@ export default {
   methods: {
     indexContacts: function () {
       axios.get("/contacts").then((response) => {
-        console.log("Photos index", response.data);
+        console.log("Contacts index", response.data);
         this.contacts = response.data;
+      });
+    },
+    createContacts: function () {
+      axios.post("/contacts.json", this.newContactParams).then((response) => {
+        console.log("Contacts create", response);
+        this.contacts.push(response.data);
+        this.newContactParams = {};
       });
     },
   },
